97ddaf02aa3662855e7f4686a127b06d059913b4
AimanFariz <aimanfarizz33@gmail.com>
2026-05-02 19:26:17 -0500

    force dynamic

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 // app/dashboard/page.tsx
 "use client";
-
+export const dynamic = "force-dynamic";
 import { useSearchParams } from "next/navigation";
 import { useState } from "react";
 import Header from "../../components/Header";
